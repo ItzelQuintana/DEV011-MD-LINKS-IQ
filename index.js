4a8b84c675ba5b4539f90bd1e45167e90cf61d61
ItzelQuintana <ovalli0326@gmail.com>
2023-11-28 20:46:52 -0600

    cambios al 28/11

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = () => {
-  // ...
-};
+  //Identifica si la ruta existe
+  const mdLinks = (path) => {
+    return new Promise((resolve, reject) => {
+    resolve()
+      
+    });
+  }
+
+  module.exports = {mdLinks}
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,13 @@
-const mdLinks = require('../');
+const {mdLinks} = require('../');
 
 
 describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
+  });
+  it('Debería devolver una promesa', () => {
+    expect(mdLinks()).toBe(typeof Promise)
   });
 
 });
