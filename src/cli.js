f772de1c19b34e557a184a052f55f442de1d258c
ItzelQuintana <ovalli0326@gmail.com>
2024-01-05 08:19:25 -0600

    Modulación correcta como biblioteca

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const { mdLinks, stats, validateLinks, statsWithValidate } = require('./functions');
 
 function processCLI(filePath, validate, statsOption) {
@@ -24,6 +25,22 @@ function processCLI(filePath, validate, statsOption) {
     .catch(error => {
       console.error(error);
     });
+}
+
+// Verifica si se está ejecutando desde la línea de comandos
+if (require.main === module) {
+  // Obtén los argumentos de la línea de comandos
+  const [, , filePath, ...flags] = process.argv;
+
+  // Convierte los flags en un conjunto para facilitar la verificación
+  const flagSet = new Set(flags);
+
+  // Convierte los flags en booleanos
+  const validate = flagSet.has("--validate");
+  const statsOption = flagSet.has("--stats");
+
+  // Llama a la función principal
+  processCLI(filePath, validate, statsOption);
 }
 
 module.exports = { processCLI };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const { processCLI } = require('./cli');
 
-const filePath = process.argv[2] || 'README.md';  // Cambio aquí para permitir una ruta dinámica
-const validate = process.argv.includes("--validate");
-const statsOption = process.argv.includes("--stats");
+function mdLinks(filePath, options = { validate: false, stats: false }) {
+  const { validate, stats } = options;
+  processCLI(filePath, validate, stats);
+}
 
-processCLI(filePath, validate, statsOption);
+module.exports = { mdLinks };
